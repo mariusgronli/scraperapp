01434b8d3589733c7d84eac71acfc5331b1dd0bf
mariusgronli <mariusgronli@gmail.com>
2020-09-30 21:25:16 +0100

    fixed sorting in chart function

--- scraper/views_functions.py
+++ scraper/views_functions.py
@@ -66,7 +66,7 @@ def populate_price_change_graph(market):
     data = list()
     labels = list()
 
-    queryset = DailyStatistic.objects.filter(market=market).order_by('-date')[:10]
+    queryset = DailyStatistic.objects.filter(market=market).order_by('date')[:10]
 
     for stat in queryset:
         data.append(round(stat.percent_change_dd*100))
