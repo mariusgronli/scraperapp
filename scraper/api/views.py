b2ca014d19b06349a5b36647937b7b2f3d65ed7c
mariusgronli <mariusgronli@gmail.com>
2021-07-03 20:06:43 +0100

    Ignores more jupyter files

--- scraper/api/views.py
+++
@@ -1,7 +0,0 @@
-
-from rest_framework.generics import get_object_or_404
-from .serializers import HouseSerializer,MarketSerializer
-from rest_framework.response import Response
-from scraper.models import MarketModel,HouseModel,DailyStatistic,AverageModel
-
-################ Market model #####################
